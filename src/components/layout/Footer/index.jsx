a20708d24d33043fb08236148f1935d592557cab
Robert Nilsen <padletut@gmail.com>
2026-01-14 23:23:56 +0100

    Refactor Footer component to use Bootstrap Container for improved layout

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -1,5 +1,11 @@
+import { Container } from "react-bootstrap";
+
 function Footer() {
-  return <footer>Website footer</footer>;
+  return (
+    <footer className="bg-light">
+      <Container>Website footer</Container>
+    </footer>
+  );
 }
 
 export default Footer;
