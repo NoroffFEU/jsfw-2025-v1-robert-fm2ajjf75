b43a38103883cb7b054a8c9eeeb34dd1eca6ad9c
Robert Nilsen <padletut@gmail.com>
2026-01-14 22:22:54 +0100

    Add layout components: Header, Footer, and Outlet

---
+++ src/components/layout/Header/index.jsx
@@ -0,0 +1,12 @@
+import HeaderNavBar from "../../HeaderNavbar";
+
+function Header() {
+  return (
+    <header>
+      <div>Header with Logo and nav</div>
+      <HeaderNavBar />
+    </header>
+  );
+}
+
+export default Header;
