c31181075b4fc74e9035c1a7be50f488af6f12fc
Robert Nilsen <padletut@gmail.com>
2026-01-15 23:26:55 +0100

    Add getDiscountInfo utility function to calculate discount details

---
+++ src/utils/getdiscountinfo.js
@@ -0,0 +1,13 @@
+function getDiscountInfo(price, discountPrice) {
+  if (!discountPrice) {
+    return null;
+  }
+  const discountAmount = price - discountPrice;
+  const discountPercentage = ((discountAmount / price) * 100).toFixed(2);
+  return {
+    discountAmount: discountAmount.toFixed(2),
+    discountPercentage: discountPercentage,
+  };
+}
+
+export default getDiscountInfo;
