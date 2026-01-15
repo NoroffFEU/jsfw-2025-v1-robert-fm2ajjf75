12bca7407eaebb628d072cdad030ad9138a2f6ba
Robert Nilsen <padletut@gmail.com>
2026-01-15 23:24:58 +0100

    Refactor App component to use ProductDetailRenderer and wrap in CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import "./App.css";
 import Layout from "./components/layout";
 import Home from "./pages/Home/page";
-import ProductDetail from "./pages/ProductDetail/page";
+import ProductDetailRenderer from "./pages/ProductDetail/page";
 import Contact from "./pages/Contact/page";
 import Checkout from "./pages/Checkout/page";
 import CheckoutSuccess from "./pages/CheckoutSuccess/page";
 import RouteNotFound from "./pages/RouteNotFound/page";
 import { ProductsProvider } from "./context/Products";
+import { CartProvider } from "./context/Cart";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ProductsProvider>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="product/:id" element={<ProductDetail />} />
-            <Route path="Contact" element={<Contact />} />
-            <Route path="Checkout" element={<Checkout />} />
-            <Route path="CheckoutSuccess" element={<CheckoutSuccess />} />
-            <Route path="*" element={<RouteNotFound />} />
-          </Route>
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="product/:id" element={<ProductDetailRenderer />} />
+              <Route path="Contact" element={<Contact />} />
+              <Route path="cart" element={<Checkout />} />
+              <Route path="CheckoutSuccess" element={<CheckoutSuccess />} />
+              <Route path="*" element={<RouteNotFound />} />
+            </Route>
+          </Routes>
+        </div>
+      </CartProvider>
     </ProductsProvider>
   );
 }
