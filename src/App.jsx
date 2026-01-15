dc7e9de7790547c2cf1a51c7469da0408880cd8c
Robert Nilsen <padletut@gmail.com>
2026-01-15 01:34:55 +0100

    Wrap App component with ProductsProvider for context management, route product/:id to correct page

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import Contact from "./pages/Contact/page";
 import Checkout from "./pages/Checkout/page";
 import CheckoutSuccess from "./pages/CheckoutSuccess/page";
 import RouteNotFound from "./pages/RouteNotFound/page";
+import { ProductsProvider } from "./context/Products";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="ProductDetail" element={<ProductDetail />} />
-          <Route path="Contact" element={<Contact />} />
-          <Route path="Checkout" element={<Checkout />} />
-          <Route path="CheckoutSuccess" element={<CheckoutSuccess />} />
-          <Route path="*" element={<RouteNotFound />} />
-        </Route>
-      </Routes>
-    </div>
+    <ProductsProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="product/:id" element={<ProductDetail />} />
+            <Route path="Contact" element={<Contact />} />
+            <Route path="Checkout" element={<Checkout />} />
+            <Route path="CheckoutSuccess" element={<CheckoutSuccess />} />
+            <Route path="*" element={<RouteNotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </ProductsProvider>
   );
 }
 
