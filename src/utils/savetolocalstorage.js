68cbabba28e882c4bdc67a737fb110d10266a54e
Robert Nilsen <padletut@gmail.com>
2026-01-16 22:01:13 +0100

    Removed loadFromLocalStorage and saveToLocalStorage

--- src/utils/savetolocalstorage.js
+++
@@ -1,8 +0,0 @@
-export function saveToLocalStorage(key, value) {
-  try {
-    const serializedValue = JSON.stringify(value);
-    localStorage.setItem(key, serializedValue);
-  } catch (error) {
-    console.error("Error saving to localStorage", error);
-  }
-}
